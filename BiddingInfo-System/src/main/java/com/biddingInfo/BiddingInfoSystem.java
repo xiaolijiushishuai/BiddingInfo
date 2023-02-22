9174ed364beefdf977767bf55c467353755b345d
xiaolijiushishuai <836075337@qq.com>
2023-02-22 21:25:42 +0800

    VER202302branch

---
+++ BiddingInfo-System/src/main/java/com/biddingInfo/BiddingInfoSystem.java
@@ -0,0 +1,15 @@
+package com.biddingInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiddingInfoSystem {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiddingInfoSystem.class, args);
+
+
+    }
+
+}
